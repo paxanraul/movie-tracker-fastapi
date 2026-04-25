67a006b777ea6db4e6dbc3dd6eebb1f44065f0da
paxanraul <raul20081033@gmail.com>
2026-04-25 21:37:15 +0300

    create postgres db

--- back/db.py
+++ back/db.py
@@ -1,1 +1,12 @@
-# will soon!
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/movie_db"
+
+engine = create_async_engine(DATABASE_URL)
+
+SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
