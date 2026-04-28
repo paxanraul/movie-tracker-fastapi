446dbb339c090d64448c6a919862fbc4c2b74bc3
paxanraul <raul20081033@gmail.com>
2026-04-28 15:09:39 +0300

    create movie table

--- back/db.py
+++ back/db.py
@@ -1,7 +1,8 @@
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/movie_db"
+DATABASE_URL = "postgresql+asyncpg://paxan_raul:123456@localhost:5432/movie_db"
 
 engine = create_async_engine(DATABASE_URL)
 
@@ -10,3 +11,10 @@ SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
 class Base(DeclarativeBase):
     pass
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    year = Column(Integer)
