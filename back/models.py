c0ad5011709ed9c1a1fefd578d0443ad3823c9f6
paxanraul <raul20081033@gmail.com>
2026-05-03 22:16:43 +0300

    add input field

--- back/models.py
+++ back/models.py
@@ -1,11 +1,20 @@
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Mapped, mapped_column
 
 from db import Base
 
-# Создает таблицу с фильмами
 class Movie(Base):
     __tablename__ = "movies"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(nullable=False)
     year: Mapped[int] = mapped_column()
+
+class MovieCreate(BaseModel):
+    title: str = Field(..., example="Batman")
+    year: int = Field(..., example=2008)
+
+
+class MovieUpdate(BaseModel):
+    title: str | None = Field(None, example="Batman")
+    year: int | None = Field(None, example=2008)
