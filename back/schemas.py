f9ed51a1775fd7291b228e2eb06d872bfd9033a2
paxanraul <raul20081033@gmail.com>
2026-05-05 21:30:11 +0300

    update schemas.py

--- back/schemas.py
+++ back/schemas.py
@@ -1,1 +1,10 @@
-# will soon!
+from pydantic import BaseModel, Field
+
+class MovieCreate(BaseModel):
+    title: str = Field(..., example="Batman")
+    year: int = Field(..., example=2008)
+
+
+class MovieUpdate(BaseModel):
+    title: str | None = Field(None, example="Batman")
+    year: int | None = Field(None, example=2008)
