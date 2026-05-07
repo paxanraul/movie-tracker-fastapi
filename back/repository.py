64f3d0b21a466f83b287d2b919f9d7b4edab774a
paxanraul <raul20081033@gmail.com>
2026-05-07 16:17:46 +0300

    now it's working

--- back/repository.py
+++ back/repository.py
@@ -1,1 +1,48 @@
-# will soon!
+from sqlalchemy import select
+
+from models import Movie
+from schemas import MovieUpdate, MovieCreate
+
+async def get_movies(session):
+    result = await session.execute(select(Movie))
+    return result.scalars().all()
+
+async def get_movie_id(session, movie_id: int):
+    result = await session.execute(select(Movie).where(Movie.id == movie_id))
+    return result.scalars().one_or_none()
+
+async def add_movie(session, item: MovieCreate):
+    new_movie = Movie(title=item.title, year=item.year)
+    session.add(new_movie)
+    await session.commit()
+    return new_movie
+
+async def delete_movie(session, movie_id: int):
+    result = await session.execute(
+        select(Movie).where(Movie.id == movie_id)
+    )
+    movie = result.scalars().one_or_none()
+
+    if movie is None:
+        return None
+
+    await session.delete(movie)
+    await session.commit()
+    return movie
+
+async def update_movie(session, movie_id: int, item: MovieUpdate):
+    result = await session.execute(
+        select(Movie).where(Movie.id == movie_id)
+    )
+    movie = result.scalars().one_or_none()
+
+    if movie is None:
+        return None
+
+    if item.title is not None:
+        movie.title = item.title
+    if item.year is not None:
+        movie.year = item.year
+
+    await session.commit()
+    return movie
