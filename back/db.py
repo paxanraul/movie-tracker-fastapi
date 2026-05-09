7128254317c5384c4f0bbedd173a0d64274536c5
paxanraul <raul20081033@gmail.com>
2026-05-09 16:29:10 +0300

    connect to docker

--- back/db.py
+++ back/db.py
@@ -1,7 +1,12 @@
+import os
+
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-DATABASE_URL = "postgresql+asyncpg://paxan_raul:123456@localhost:5432/movie_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+asyncpg://paxan_raul:123456@localhost:5432/movie_db",
+)
 
 engine = create_async_engine(DATABASE_URL)
 
