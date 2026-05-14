d9f2e97a4f313c8c0b04cd89d3b794d9d7c35315
paxanraul <raul20081033@gmail.com>
2026-05-14 13:51:05 +0300

    new classes for auth

--- back/schemas.py
+++ back/schemas.py
@@ -8,3 +8,25 @@ class MovieCreate(BaseModel):
 class MovieUpdate(BaseModel):
     title: str | None = Field(None, json_schema_extra="Batman")
     year: int | None = Field(None, json_schema_extra=2008)
+
+
+class UserRegister(BaseModel):
+    username: str
+    password: str
+
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+
+    model_config = {"from_attributes": True}
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
