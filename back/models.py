28d0ec36ddd1e88c5a4317987b7efacf0d3e2a20
paxanraul <raul20081033@gmail.com>
2026-05-14 13:50:34 +0300

    a bit change

--- back/models.py
+++ back/models.py
@@ -11,6 +11,7 @@ class Movie(Base):
 
 class User(Base):
     __tablename__ = "users"
+
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    username: Mapped[str] = mapped_column()
+    username: Mapped[str] = mapped_column(unique=True, index=True)
     hashed_password: Mapped[str] = mapped_column(nullable=False)
